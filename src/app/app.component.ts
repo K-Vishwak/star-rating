f70b33b1fc7f868124930522950cbd0fe42ec068
K-Vishwak <vishwak.kurapati@gmail.com>
2022-08-14 09:52:42 +0530

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, VERSION } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
@@ -6,16 +6,41 @@ import { Component, VERSION } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'Angular Star Rating';
+  title = 'Star Rating';
 
   arr: any[] = [];
   index: number = 0;
 
   constructor() {
-    this.arr = [1, 2, 3, 4, 5];
+    this.arr = [
+      { value: 1, color: 'black' },
+      { value: 2, color: 'black' },
+      { value: 3, color: 'black' },
+      { value: 4, color: 'black' },
+      { value: 5, color: 'black' },
+    ];
   }
 
-  onClickItem(index) {
+  onClickItem(index: number) {
     this.index = index;
+    this.manipulateArr(index);
+  }
+
+  onMouseHover(index: number) {
+    this.manipulateArr(index);
+  }
+
+  onMouseLeave() {
+    this.manipulateArr(this.index);
+  }
+
+  manipulateArr(index) {
+    this.arr.forEach((item, i) => {
+      if (i <= index - 1) {
+        item.color = 'green';
+      } else {
+        item.color = 'black';
+      }
+    });
   }
 }
